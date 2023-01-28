023fb18b9bc8050ddd823c7d7b90b0c1f5d97d8e
Srikant0052 <srikant0052@gmail.com>
2023-01-28 17:28:03 +0530

    change socket url

--- src/components/socket-io-connection/socket.js
+++ src/components/socket-io-connection/socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-export const socket = io.connect("https://playnwin.fun/api", {
+export const socket = io.connect("https://playnwin.fun", {
   cors: "*",
   transports: ["websocket"],
 });
